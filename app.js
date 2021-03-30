fdad17d3b2a11f7d20b4b3fbaa9580bcf3bcb025
aziv07 <aziv07@gmail.com>
2021-03-31 01:32:50 +0200

    fix

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express')
 require('dotenv').config()
-const connectDB = require('./db.js')
+//const connectDB = require('./db.js')
 const colors = require('colors/safe')
 const app = express()
 const hrsRoutes = require('./routes/hr')
 
-connectDB(process.env.DB)
+//connectDB(process.env.DB)
 app.use(express.json({ extended: false }))
 
 app.get('/api', (req, res) => res.json({ msg: 'welcome to ur endpoint!' }))
