b87729753c4416c25cd4cec3ced7cf0bc46e5157
aziv07 <aziv07@gmail.com>
2021-04-02 16:18:40 +0200

    mailing service + planed mail

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require('express')
 require('dotenv').config()
-//const connectDB = require('./db.js')
+const connectDB = require('./db.js')
 const colors = require('colors/safe')
 const app = express()
 const hrsRoutes = require('./routes/hr')
-
-//connectDB(process.env.DB)
+const mailingRoutes = require('./routes/emailing')
+connectDB(process.env.DB)
 app.use(express.json({ extended: false }))
 
 app.get('/api', (req, res) => res.json({ msg: 'welcome to ur endpoint!' }))
 app.use('/api/hrs', hrsRoutes)
+app.use('/api/email', mailingRoutes)
 const port = process.env.PORT || 5000
 app.listen(port, () =>
   console.log(colors.green('server started on PORT :' + port))
---
+++ routes/emailing.js
@@ -0,0 +1,42 @@
+const mailer = require('nodemailer')
+const express = require('express')
+const schedule = require('node-schedule')
+const router = express.Router()
+const transporter = mailer.createTransport({
+  service: 'hotmail',
+  auth: { user: 'gachi1231@outlook.com', pass: '13051998gachi' },
+})
+const options = {
+  from: 'gachi1231@outlook.com',
+  to: 'aziv07@gmail.com',
+  subject: 'email from node.js',
+  text: 'you got this!',
+}
+
+router.post('/', async (req, res) => {
+  transporter.sendMail(options, (err, info) => {
+    try {
+      if (err) throw new Error(err)
+      else res.json(info.response)
+    } catch (error) {
+      res.status(500).json(err)
+    }
+  })
+})
+
+router.post('/schedule', async (req, res) => {
+  transporter.sendMail(options, (err, info) => {
+    try {
+      const job = schedule.scheduleJob('17 * * * *', function () {
+        transporter.sendMail(options, (err, info) => {
+          if (err) throw new Error(err)
+        })
+      })
+      res.json({ msg: 'mail planed' })
+    } catch (error) {
+      res.status(500).json(err)
+    }
+  })
+})
+
+module.exports = router
