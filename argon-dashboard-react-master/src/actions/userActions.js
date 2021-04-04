38f8108e641b08bcb1f51842199e215af4e1c5d4
amineZargouni <mohamedamine.zargouni@esprit.tn>
2021-04-04 21:43:35 +0100

    user constants + user Reducer + user Action

---
+++ argon-dashboard-react-master/src/actions/userActions.js
@@ -0,0 +1,55 @@
+import axios from 'axios'
+import {
+    LOGIN_REQUEST,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    CLEAR_ERRORS
+
+} from '../constants/userConstants'
+
+
+//login request
+
+export const login = (email, password) => async (dispatch) => {
+
+    try {
+
+        dispatch({
+            type: LOGIN_REQUEST
+        })
+
+        const config = {
+            headers: {
+                'Content-Type' : 'application/json'
+            }
+        }
+
+        const {data} = await axios.post('/login' , {email,password},config)
+
+        dispatch({
+            type: LOGIN_SUCCESS,
+            payload: data.user
+        })
+
+
+
+
+    } catch (error) {
+        dispatch({
+            type: LOGIN_FAIL,
+            payload: error.response.data.message
+        })
+    }
+
+}
+
+
+//clear errors
+
+export const clearErrors = () => async(dispatch) => {
+
+    dispatch({
+        type: CLEAR_ERRORS
+    })
+
+}
