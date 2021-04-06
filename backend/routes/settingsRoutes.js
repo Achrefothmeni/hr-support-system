8d54d2df1d0d43ca8fa68ed39af88fc3c9de8132
Achrefothmeni <othmeniachref@gmail.com>
2021-04-06 22:57:12 +0100

    deleted backend folder

--- backend/routes/settingsRoutes.js
+++
@@ -1,7 +0,0 @@
-import  express from 'express'
-import {getSettings , createSettings} from '../controller/settingsController.js'
-const router = express.Router()
-router.get('/',getSettings)
-
-
-export default router
