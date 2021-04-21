9b0e46f4decdd1bd383f93fa4d780c7e3071771d
aziv07 <aziv07@gmail.com>
2021-04-21 05:57:44 +0100

    Agenda setup

--- argon-dashboard-react-master/src/views/examples/PlanMeets.js
+++ argon-dashboard-react-master/src/views/examples/PlanMeets.js
@@ -37,7 +37,10 @@ import TextField from '@material-ui/core/TextField'
 
 import UserHeader from 'components/Headers/UserHeader.js'
 import { planforMeet, getMeets } from '../../actions/meetAction'
-
+import Scheduler from 'devextreme-react/scheduler'
+import notify from 'devextreme/ui/notify'
+import 'devextreme/dist/css/dx.common.css'
+import 'devextreme/dist/css/dx.light.css'
 const useStyles = makeStyles((theme) => ({
   container: {
     display: 'flex',
@@ -51,12 +54,25 @@ const useStyles = makeStyles((theme) => ({
 }))
 
 const PlanMeets = ({ history }) => {
+  const [data, setData] = useState([])
+  const editing = {
+    allowAdding: false,
+    allowDeleting: true,
+    allowResizing: true,
+    allowDragging: false,
+    allowUpdating: false,
+  }
+  const currentDate = new Date()
+  const views = ['day', 'week']
+
   const dispatch = useDispatch()
   const classes = useStyles()
   const { isAuthenticated, error, loading, user } = useSelector(
     (state) => state.auth
   )
-
+  const { meets, error: meetError, loading: meetLoading } = useSelector(
+    (state) => state.meet
+  )
   let today = new Date()
   const [meet, setMeet] = useState({
     day: {
@@ -78,7 +94,20 @@ const PlanMeets = ({ history }) => {
       console.log('not admin')
       history.push('/auth/login')
     }
-  }, [dispatch, isAuthenticated, error, history])
+    if (!meets || meets.length == 0) dispatch(getMeets())
+    const arr = []
+    meets.map((m) => {
+      const d = new Date(m.planedFor)
+      d.setHours(d.getHours() + 1)
+
+      arr.push({
+        text: `Planed meet with ${m.email}`,
+        startDate: new Date(m.planedFor),
+        endDate: d,
+      })
+    })
+    setData(arr)
+  }, [dispatch, isAuthenticated, error, history, meets])
   const addMeet = (e) => {
     e.preventDefault()
     const test = new Date(meet.day.value) - new Date(Date.now()) > 0
@@ -323,97 +352,16 @@ const PlanMeets = ({ history }) => {
                 <hr className='my-4' />
 
                 <Col md={12}>
-                  <Agenda
-                    // The list of items that have to be displayed in agenda. If you want to render item as empty date
-                    // the value of date key has to be an empty array []. If there exists no value for date key it is
-                    // considered that the date in question is not yet loaded
-                    items={{
-                      '2012-05-22': [{ name: 'item 1 - any js object' }],
-                      '2012-05-23': [
-                        { name: 'item 2 - any js object', height: 80 },
-                      ],
-                      '2012-05-24': [],
-                      '2012-05-25': [
-                        { name: 'item 3 - any js object' },
-                        { name: 'any js object' },
-                      ],
-                    }}
-                    // Callback that gets called when items for a certain month should be loaded (month became visible)
-                    loadItemsForMonth={(month) => {
-                      console.log('trigger items loading')
-                    }}
-                    // Callback that fires when the calendar is opened or closed
-                    onCalendarToggled={(calendarOpened) => {
-                      console.log(calendarOpened)
-                    }}
-                    // Callback that gets called on day press
-                    onDayPress={(day) => {
-                      console.log('day pressed')
-                    }}
-                    // Callback that gets called when day changes while scrolling agenda list
-                    onDayChange={(day) => {
-                      console.log('day changed')
-                    }}
-                    // Initially selected day
-                    selected={'2012-05-16'}
-                    // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
-                    minDate={'2012-05-10'}
-                    // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
-                    maxDate={'2012-05-30'}
-                    // Max amount of months allowed to scroll to the past. Default = 50
-                    pastScrollRange={50}
-                    // Max amount of months allowed to scroll to the future. Default = 50
-                    futureScrollRange={50}
-                    // Specify how each item should be rendered in agenda
-                    renderItem={(item, firstItemInDay) => {
-                      return <View />
-                    }}
-                    // Specify how each date should be rendered. day can be undefined if the item is not first in that day.
-                    renderDay={(day, item) => {
-                      return <View />
-                    }}
-                    // Specify how empty date content with no items should be rendered
-                    renderEmptyDate={() => {
-                      return <View />
-                    }}
-                    // Specify how agenda knob should look like
-                    renderKnob={() => {
-                      return <View />
-                    }}
-                    // Specify what should be rendered instead of ActivityIndicator
-                    renderEmptyData={() => {
-                      return <View />
-                    }}
-                    // Specify your item comparison function for increased performance
-                    rowHasChanged={(r1, r2) => {
-                      return r1.text !== r2.text
-                    }}
-                    // Hide knob button. Default = false
-                    hideKnob={true}
-                    // By default, agenda dates are marked if they have at least one item, but you can override this if needed
-                    markedDates={{
-                      '2012-05-16': { selected: true, marked: true },
-                      '2012-05-17': { marked: true },
-                      '2012-05-18': { disabled: true },
-                    }}
-                    // If disabledByDefault={true} dates flagged as not disabled will be enabled. Default = false
-                    disabledByDefault={true}
-                    // If provided, a standard RefreshControl will be added for "Pull to Refresh" functionality. Make sure to also set the refreshing prop correctly.
-                    onRefresh={() => console.log('refreshing...')}
-                    // Set this true while waiting for new data from a refresh
-                    refreshing={false}
-                    // Add a custom RefreshControl component, used to provide pull-to-refresh functionality for the ScrollView.
-                    refreshControl={null}
-                    // Agenda theme
-                    theme={{
-                      ...calendarTheme,
-                      agendaDayTextColor: 'yellow',
-                      agendaDayNumColor: 'green',
-                      agendaTodayColor: 'red',
-                      agendaKnobColor: 'blue',
-                    }}
-                    // Agenda container style
-                    style={{}}
+                  <Scheduler
+                    timeZone='Africa/Tunis'
+                    dataSource={data}
+                    views={views}
+                    defaultCurrentView='week'
+                    defaultCurrentDate={currentDate}
+                    startDayHour={8}
+                    endDayHour={19}
+                    height={600}
+                    editing={editing}
                   />
                 </Col>
               </CardBody>
